0abd1baa3e94e60dfa9eb33710310ddfee6dc299
yashvikotadiya04 <96778187+yashvikotadiya04@users.noreply.github.com>
2022-04-19 18:30:51 +0530

    Add files via upload

---
+++ prac4.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+
+num = [int(n) for n in input("Enter an array: ").split()]
+k = int(input())
+cnt = 0
+for i in range(1, len(num)+1):
+    for c in combinations(num, i):
+        if sum(c) == k:
+            cnt += 1
+print(cnt)
+
